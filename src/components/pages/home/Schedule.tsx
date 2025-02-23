5d71b905805718cfa8888d7e80f5a1f0e14871e3
VanTuoi <vantuoi.dev@gmail.com>
2025-02-23 10:36:12 +0700

    feat: services in hompage

--- src/components/pages/home/Schedule.tsx
+++ src/components/pages/home/Schedule.tsx
@@ -1,46 +1,13 @@
+import ScheduleForm from "./ScheduleForm";
+
 export const Schedule = () => (
     <section className="ftco-section ftco-no-pt bg-light">
         <div className="container">
             <div className="row no-gutters">
-                <div className="col-md-12	featured-top">
+                <div className="col-md-12 featured-top">
                     <div className="row no-gutters">
                         <div className="col-md-4 d-flex align-items-center">
-                            <form action="#" className="request-form bg-primary wow fadeInUp" data-wow-delay="0.1s">
-                                <h2>Đặt lịch hẹn tư vấn hỗ trợ</h2>
-                                <div className="form-group">
-                                    <label htmlFor="fullname" className="label">
-                                        Họ và tên
-                                    </label>
-                                    <input
-                                        id="fullname"
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Nguyễn Trần Thủy Tiên"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="phone" className="label">
-                                        Số điện thoại (Có sử dụng Zalo)
-                                    </label>
-                                    <input id="phone" type="text" className="form-control" placeholder="0369369369" />
-                                </div>
-                                <div className="d-flex">
-                                    <div className="form-group mr-2">
-                                        <label htmlFor="book_pick_date" className="label">
-                                            Ghi chú
-                                        </label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            id="book_pick_date"
-                                            placeholder="Loại đồ án,..."
-                                        />
-                                    </div>
-                                </div>
-                                <div className="form-group">
-                                    <input type="submit" value="Đặt lịch hẹn" className="btn btn-secondary py-3 px-3" />
-                                </div>
-                            </form>
+                            <ScheduleForm />
                         </div>
                         <div className="col-md-8 d-flex align-items-center">
                             <div className="services-wrap rounded-right w-100">
@@ -61,7 +28,7 @@ export const Schedule = () => (
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-md-4 d-flex align-self-stretch ">
+                                    <div className="col-md-4 d-flex align-self-stretch">
                                         <div className="services w-100 text-center">
                                             <div className="icon d-flex align-items-center justify-content-center">
                                                 <i
@@ -74,7 +41,7 @@ export const Schedule = () => (
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-md-4 d-flex align-self-stretch ">
+                                    <div className="col-md-4 d-flex align-self-stretch">
                                         <div className="services w-100 text-center">
                                             <div className="icon d-flex align-items-center justify-content-center">
                                                 <i className="fa fa-phone fa-3x text-primary" aria-hidden="true"></i>
@@ -93,3 +60,5 @@ export const Schedule = () => (
         </div>
     </section>
 );
+
+export default Schedule;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/* eslint-disable no-undef */
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "./src")
+        }
+    }
 });
