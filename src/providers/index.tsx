6b5391f2890825ae573064796471ad9a1b2109b4
VanTuoi <vantuoi.dev@gmail.com>
2025-03-31 19:18:12 +0700

    fix: temporary fix interface error with bootstrap

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,6 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { PropsWithChildren } from "react";
 import "~/styles/style.css";
