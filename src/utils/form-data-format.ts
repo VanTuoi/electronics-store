dc35b193f46dd9669d4599bcc0e84b4157aedb92
VanTuoi <vantuoi.dev@gmail.com>
2025-04-16 16:20:18 +0700

    feat: update feature admin products and complete image processing

--- src/utils/form-data-format.ts
+++ src/utils/form-data-format.ts
@@ -1,12 +1,21 @@
 type FormDataValue = string | Blob | File;
-type FormDataObject = Record<string, FormDataValue>;
+type FormDataObject = Record<string, FormDataValue | FormDataValue[]>;
 
 export const formDataToObject = (formData: FormData): FormDataObject => {
     const result: FormDataObject = {};
+
     formData.forEach((value, key) => {
-        if (typeof value === "string" || value instanceof Blob) {
-            result[key] = value;
+        const isFile = value instanceof Blob;
+
+        if (result[key]) {
+            if (!Array.isArray(result[key])) {
+                result[key] = [result[key] as FormDataValue];
+            }
+            (result[key] as FormDataValue[]).push(value);
+        } else {
+            result[key] = isFile ? [value] : value;
         }
     });
+
     return result;
 };
