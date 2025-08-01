2f67f4d589bb853f57a72a22e790f4e2885fa279
VanTuoi <vantuoi.dev@gmail.com>
2025-08-01 19:01:34 +0700

    fix: fix layout error when changing pages

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "~/styles/style.scss";
 
 import { memo, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
