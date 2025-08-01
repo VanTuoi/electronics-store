8f090bfd33b292f15e18693c7457e0aa7b609dec
VanTuoi <vantuoi.dev@gmail.com>
2025-08-01 23:13:11 +0700

    chore: fix login and background image error

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,36 @@
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 import { Helmet } from "react-helmet";
+import { useNavigate } from "react-router-dom";
 
 import { LoginForm } from "~/components/pages";
+import { userAtom } from "~/stores";
 
-const Login = () => (
-  <div className="login-page">
-    <Helmet>
-      <title>Đăng nhập | Electronics Store</title>
-      <meta
-        name="description"
-        content="Đăng nhập để truy cập tài khoản của bạn và trải nghiệm các dịch vụ từ Electronics Store."
-      />
-    </Helmet>
+const Login = () => {
+  const navigate = useNavigate();
+  const [user] = useAtom(userAtom);
 
-    <div className="container login-page-form min-vh-100 d-flex align-items-center justify-content-center">
-      <LoginForm />
+  useEffect(() => {
+    if (user?.user?.role === "admin") {
+      navigate("/admin");
+    }
+  }, [user, navigate]);
+
+  return (
+    <div className="login-page">
+      <Helmet>
+        <title>Đăng nhập | Electronics Store</title>
+        <meta
+          name="description"
+          content="Đăng nhập để truy cập tài khoản của bạn và trải nghiệm các dịch vụ từ Electronics Store."
+        />
+      </Helmet>
+
+      <div className="container login-page-form min-vh-100 d-flex align-items-center justify-content-center">
+        <LoginForm />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
