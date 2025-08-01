fdb4a17d3752bde02e6c7f70a59baa512c7105e5
VanTuoi <vantuoi.dev@gmail.com>
2025-08-01 22:43:22 +0700

    fix: fix syntax error

--- src/pages/admin/categories.tsx
+++ src/pages/admin/categories.tsx
@@ -1,16 +1,10 @@
 import { memo } from "react";
-import { Helmet } from "react-helmet";
 
 import { CategoriesTable, PageBreadcrumb, PageMeta } from "~/components/admin";
 
 const Categories = memo(() => (
   <>
-    <Helmet>
-      <title>Danh mục tủ điện | Admin</title>
-      <meta name="description" content="Quản lý danh mục các loại tủ điện trong hệ thống." />
-    </Helmet>
-
-    <PageMeta title="Danh mục tủ điện" description="" />
+    <PageMeta title="Danh mục tủ điện" description="Quản lý danh mục các loại tủ điện trong hệ thống" />
     <PageBreadcrumb pageTitle="Danh mục tủ điện" />
     <div className="space-y-6">
       <CategoriesTable />
